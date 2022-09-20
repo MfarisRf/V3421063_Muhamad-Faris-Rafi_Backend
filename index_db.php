999aabf8a7919de279d4c18735c58a020d88fdf2
MUHAMAD FARIS RAFI <farisrafi2002@student.uns.ac.id>
2022-09-20 21:06:51 +0700

    Ubah

--- index_db.php
+++ index_db.php
@@ -271,10 +271,10 @@ class penduduk{
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="dist/img/fotofaris1.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Mohammad Jose Rizal</a>
+          <a href="#" class="d-block">Muhamad Faris Rafi</a>
         </div>
       </div>
 
